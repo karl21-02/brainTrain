41eee6d02960cdfe69c5354c4971847830c9d9d7
kimjunhee <manuna530@gmail.com>
2024-12-15 13:55:09 +0900

    Initial commit

---
+++ app/src/main/java/com/example/braintrain/ui/landingPage/LandingPage.java
@@ -0,0 +1,23 @@
+package com.example.braintrain.ui.landingPage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.braintrain.MainActivity;
+import com.example.braintrain.R;
+
+public class LandingPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(LandingPage.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 5000);
+    }
+}
